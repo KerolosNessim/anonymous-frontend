5e47b1149b38d0bce8c7a1ee4ba5f370afbd4ee9
KerolosNessim <keronessim9@gmail.com>
2025-07-01 21:18:06 +0300

    handeled auth links

--- src/components/shared/auth-buttons.jsx
+++ src/components/shared/auth-buttons.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const AuthButtons = () => {
   return (
     <div className=' flex items-center gap-2'>
-      <Link href={'/login'} className={` flex items-center justify-center w-fit   font-bold font-[family-name:var(--font-fira)]  px-4 py-2 border-2 border-primary bg-primary hover:bg-transparent hover:text-white rounded-md transition-all duration-300  `}>Sign Up</Link>
+      <Link href={'/sign-up'} className={` flex items-center justify-center w-fit   font-bold font-[family-name:var(--font-fira)]  px-4 py-2 border-2 border-primary bg-primary hover:bg-transparent hover:text-white rounded-md transition-all duration-300  `}>Sign Up</Link>
       <Link href={'/login'} className={` flex items-center justify-center w-fit   font-bold font-[family-name:var(--font-fira)]  px-4 py-2  border-2 border-primary hover:bg-primary hover:text-white rounded-md transition-all duration-300 `}>Login</Link>
       
     </div>
