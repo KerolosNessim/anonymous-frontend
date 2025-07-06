5e0e8e23f5873c234ab892354c72f4a1d00c76dc
KerolosNessim <keronessim9@gmail.com>
2025-07-06 12:39:24 +0300

    handled error in deployed

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,7 @@ const nextConfig = {
     legacyBrowsers: false, 
     optimizeCss: true, 
   },
-  browserslist: [
-    "last 2 Chrome versions",
-    "last 2 Firefox versions",
-    "last 2 Edge versions",
-    "last 2 Safari versions"
-  ],
-  transpilePackages: [],
+
 };
 
 export default nextConfig;
